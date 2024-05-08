e1fc847bd6bf48e1f9231fd9da8aecf3bde91da9
yanhuo-zhy <1760331284@qq.com>
2024-05-08 17:54:48 +0800

    Initial commit

--- protop/scripts/train_uon2.sh
+++ protop/scripts/train_uon2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 #SBATCH --account cvl
-#SBATCH -p amp48
-#SBATCH --qos amp48
+#SBATCH -p general
+#SBATCH --qos normal
 #SBATCH -N 1
 #SBATCH -c 5
 #SBATCH --mem=20000
@@ -13,99 +13,154 @@ module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
 
 source /home/psawl/miniconda3/bin/activate zhy
 
-CUDA_VISIBLE_DEVICES=0 python main.py \
-    --base_architecture="deit_base_patch16_224" \
-    --data_set="CD_CUB2011U" \
-    --data_path="/db/psawl/cub" \
-    --input_size=224 \
-    --output_dir="output_cosine/CD_CUB2011U/ablation_nocenterloss_seed(1027)" \
-    --batch_size=128 \
+CUDA_VISIBLE_DEVICES=0
+
+model=deit_base_patch16_224
+batch_size=128
+num_gpus=1
+use_port=2672
+seed=1027
+
+# Learning Rate
+warmup_lr=1e-4
+warmup_epochs=5
+features_lr=1e-4
+add_on_layers_lr=1e-3
+prototype_vectors_lr=1e-3
+
+# Optimizer & Scheduler
+opt=adamw
+sched=cosine
+decay_epochs=10
+decay_rate=0.1
+weight_decay=0.05
+epochs=200
+output_dir=output_cosine/
+input_size=224
+
+use_global=True
+use_ppc_loss=False
+last_reserve_num=196
+global_coe=0.5
+ppc_cov_thresh=1.
+ppc_mean_thresh=2.
+global_proto_per_class=5
+ppc_cov_coe=0.1
+ppc_mean_coe=0.5
+dim=768
+
+if [ "$model" = "deit_tiny_patch16_224" ]
+then
+    reserve_layer_idx=11
+elif [ "$model" = "deit_small_patch16_224" ]
+then
+    reserve_layer_idx=11
+elif [ "$model" = "deit_base_patch16_224" ]
+then
+    reserve_layer_idx=11
+elif [ "$model" = "cait_xxs24_224" ]
+then
+    reserve_layer_idx=1
+fi
+
+ft=protopformer
+data_set=CD_CUB2011U
+prototype_num=500
+data_path=/db/psawl/cub
+
+
+python main.py \
+    --base_architecture=$model \
+    --data_set=$data_set \
+    --data_path=$data_path \
+    --input_size=$input_size \
+    --output_dir=$output_dir/$data_set/"ablation_nocenterloss_seed(1027)" \
+    --batch_size=$batch_size \
     --seed=1027 \
-    --opt="adamw" \
-    --sched="cosine" \
-    --warmup-epochs=5 \
-    --warmup-lr=1e-4 \
-    --decay-epochs=10 \
-    --decay-rate=0.1 \
-    --weight_decay=0.05 \
-    --epochs=200 \
-    --finetune="protopformer" \
-    --features_lr=1e-4 \
-    --add_on_layers_lr=1e-3 \
-    --prototype_vectors_lr=1e-3 \
-    --prototype_shape 500 768 1 1 \
-    --reserve_layers 11 \
-    --reserve_token_nums 196 \
-    --use_global=True \
-    --use_ppc_loss=False \
-    --ppc_cov_thresh=1. \
-    --ppc_mean_thresh=2. \
-    --global_coe=0.5 \
-    --global_proto_per_class=5 \
-    --ppc_cov_coe=0.1 \
-    --ppc_mean_coe=0.5
+    --opt=$opt \
+    --sched=$sched \
+    --warmup-epochs=$warmup_epochs \
+    --warmup-lr=$warmup_lr \
+    --decay-epochs=$decay_epochs \
+    --decay-rate=$decay_rate \
+    --weight_decay=$weight_decay \
+    --epochs=$epochs \
+    --finetune=$ft \
+    --features_lr=$features_lr \
+    --add_on_layers_lr=$add_on_layers_lr \
+    --prototype_vectors_lr=$prototype_vectors_lr \
+    --prototype_shape $prototype_num $dim 1 1 \
+    --reserve_layers $reserve_layer_idx \
+    --reserve_token_nums $last_reserve_num \
+    --use_global=$use_global \
+    --use_ppc_loss=$use_ppc_loss \
+    --ppc_cov_thresh=$ppc_cov_thresh \
+    --ppc_mean_thresh=$ppc_mean_thresh \
+    --global_coe=$global_coe \
+    --global_proto_per_class=$global_proto_per_class \
+    --ppc_cov_coe=$ppc_cov_coe \
+    --ppc_mean_coe=$ppc_mean_coe
 
-
-CUDA_VISIBLE_DEVICES=0 python main.py \
-    --base_architecture="deit_base_patch16_224" \
-    --data_set="CD_CUB2011U" \
-    --data_path="/db/psawl/cub" \
-    --input_size=224 \
-    --output_dir="output_cosine/CD_CUB2011U/ablation_nocenterloss_seed(1028)" \
-    --batch_size=128 \
+python main.py \
+    --base_architecture=$model \
+    --data_set=$data_set \
+    --data_path=$data_path \
+    --input_size=$input_size \
+    --output_dir=$output_dir/$data_set/"ablation_nocenterloss_seed(1028)" \
+    --batch_size=$batch_size \
     --seed=1028 \
-    --opt="adamw" \
-    --sched="cosine" \
-    --warmup-epochs=5 \
-    --warmup-lr=1e-4 \
-    --decay-epochs=10 \
-    --decay-rate=0.1 \
-    --weight_decay=0.05 \
-    --epochs=200 \
-    --finetune="protopformer" \
-    --features_lr=1e-4 \
-    --add_on_layers_lr=1e-3 \
-    --prototype_vectors_lr=1e-3 \
-    --prototype_shape 500 768 1 1 \
-    --reserve_layers 11 \
-    --reserve_token_nums 196 \
-    --use_global=True \
-    --use_ppc_loss=False \
-    --ppc_cov_thresh=1. \
-    --ppc_mean_thresh=2. \
-    --global_coe=0.5 \
-    --global_proto_per_class=5 \
-    --ppc_cov_coe=0.1 \
-    --ppc_mean_coe=0.5
+    --opt=$opt \
+    --sched=$sched \
+    --warmup-epochs=$warmup_epochs \
+    --warmup-lr=$warmup_lr \
+    --decay-epochs=$decay_epochs \
+    --decay-rate=$decay_rate \
+    --weight_decay=$weight_decay \
+    --epochs=$epochs \
+    --finetune=$ft \
+    --features_lr=$features_lr \
+    --add_on_layers_lr=$add_on_layers_lr \
+    --prototype_vectors_lr=$prototype_vectors_lr \
+    --prototype_shape $prototype_num $dim 1 1 \
+    --reserve_layers $reserve_layer_idx \
+    --reserve_token_nums $last_reserve_num \
+    --use_global=$use_global \
+    --use_ppc_loss=$use_ppc_loss \
+    --ppc_cov_thresh=$ppc_cov_thresh \
+    --ppc_mean_thresh=$ppc_mean_thresh \
+    --global_coe=$global_coe \
+    --global_proto_per_class=$global_proto_per_class \
+    --ppc_cov_coe=$ppc_cov_coe \
+    --ppc_mean_coe=$ppc_mean_coe
 
-CUDA_VISIBLE_DEVICES=0 python main.py \
-    --base_architecture="deit_base_patch16_224" \
-    --data_set="CD_CUB2011U" \
-    --data_path="/db/psawl/cub" \
-    --input_size=224 \
-    --output_dir="output_cosine/CD_CUB2011U/ablation_nocenterloss_seed(1029)" \
-    --batch_size=128 \
+python main.py \
+    --base_architecture=$model \
+    --data_set=$data_set \
+    --data_path=$data_path \
+    --input_size=$input_size \
+    --output_dir=$output_dir/$data_set/"ablation_nocenterloss_seed(1029)" \
+    --batch_size=$batch_size \
     --seed=1029 \
-    --opt="adamw" \
-    --sched="cosine" \
-    --warmup-epochs=5 \
-    --warmup-lr=1e-4 \
-    --decay-epochs=10 \
-    --decay-rate=0.1 \
-    --weight_decay=0.05 \
-    --epochs=200 \
-    --finetune="protopformer" \
-    --features_lr=1e-4 \
-    --add_on_layers_lr=1e-3 \
-    --prototype_vectors_lr=1e-3 \
-    --prototype_shape 500 768 1 1 \
-    --reserve_layers 11 \
-    --reserve_token_nums 196 \
-    --use_global=True \
-    --use_ppc_loss=False \
-    --ppc_cov_thresh=1. \
-    --ppc_mean_thresh=2. \
-    --global_coe=0.5 \
-    --global_proto_per_class=5 \
-    --ppc_cov_coe=0.1 \
-    --ppc_mean_coe=0.5
+    --opt=$opt \
+    --sched=$sched \
+    --warmup-epochs=$warmup_epochs \
+    --warmup-lr=$warmup_lr \
+    --decay-epochs=$decay_epochs \
+    --decay-rate=$decay_rate \
+    --weight_decay=$weight_decay \
+    --epochs=$epochs \
+    --finetune=$ft \
+    --features_lr=$features_lr \
+    --add_on_layers_lr=$add_on_layers_lr \
+    --prototype_vectors_lr=$prototype_vectors_lr \
+    --prototype_shape $prototype_num $dim 1 1 \
+    --reserve_layers $reserve_layer_idx \
+    --reserve_token_nums $last_reserve_num \
+    --use_global=$use_global \
+    --use_ppc_loss=$use_ppc_loss \
+    --ppc_cov_thresh=$ppc_cov_thresh \
+    --ppc_mean_thresh=$ppc_mean_thresh \
+    --global_coe=$global_coe \
+    --global_proto_per_class=$global_proto_per_class \
+    --ppc_cov_coe=$ppc_cov_coe \
+    --ppc_mean_coe=$ppc_mean_coe
