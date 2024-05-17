810b6aaf68aacec1cab8ba8f833d84028e994edb
yanhuo-zhy <1760331284@qq.com>
2024-05-17 22:50:28 +0800

    add rs

--- RankStat/scripts/train_uon.sh
+++ RankStat/scripts/train_uon.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 #SBATCH --account cvl
-#SBATCH -p general
-#SBATCH --qos normal
+#SBATCH -p amp20
+#SBATCH --qos amp20
 #SBATCH -N 1
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/myocd/temp/RankStat_Animalia.txt
+#SBATCH -o /home/pszzz/hyzheng/myocd/temp/RankStat_Mollusca.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
@@ -64,8 +64,8 @@ then
 fi
 
 ft=protopformer
-data_set=Animalia
-prototype_num=195
+data_set=Mollusca
+prototype_num=235
 data_path=/db/pszzz/xxx
 
 
