e2ecd48838c437c7b067352d702ff92e2cdbb1e8
yanhuo-zhy <1760331284@qq.com>
2024-05-17 22:39:43 +0800

    add rs

--- RankStat/scripts/train_uon.sh
+++ RankStat/scripts/train_uon.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/myocd/temp/RankStat_food.txt
+#SBATCH -o /home/pszzz/hyzheng/myocd/temp/RankStat_pets.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
@@ -64,8 +64,8 @@ then
 fi
 
 ft=protopformer
-data_set=CD_food
-prototype_num=255
+data_set=CD_pets
+prototype_num=95
 data_path=/db/pszzz/xxx
 
 
