d4a87efee6d0947fec32c5752f34bbfe82d3f960
yanhuo-zhy <1760331284@qq.com>
2024-05-18 23:01:37 +0800

    wta

--- RankStat/wta.sh
+++ RankStat/wta.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/myocd/temp/rs_Mollusca.txt
+#SBATCH -o /home/pszzz/hyzheng/myocd/temp/wta_Mollusca.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
