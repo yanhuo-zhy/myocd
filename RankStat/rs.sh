3205fdc5efb851b21c17eae273a16847b482b001
yanhuo-zhy <1760331284@qq.com>
2024-05-18 18:47:05 +0800

    newsota

--- RankStat/rs.sh
+++ RankStat/rs.sh
@@ -1,6 +1,19 @@
-#!/bin/bash
+#!/bin/bash 
+#SBATCH --account cvl
+#SBATCH -p general
+#SBATCH --qos normal
+#SBATCH -N 1
+#SBATCH -c 5
+#SBATCH --mem=20000
+#SBATCH --gres gpu:1
+#SBATCH -o /home/pszzz/hyzheng/myocd/temp/rs_CD_CUB2011U.txt
 
-# 使用方式: ./run_model.sh /path/to/model.pth cub
+module load gcc/gcc-10.2.0
+module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
+
+source /home/pszzz/miniconda3/bin/activate zhy
+
+CUDA_VISIBLE_DEVICES=0
 
 MODEL_PATH='output_cosine/CD_CUB2011U/RankStat&WTA/checkpoints/epoch-best.pth'
 DATASET_NAME='CD_CUB2011U'
