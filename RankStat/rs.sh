20d8ca629d16896168e6dfa47d85ff24f04b42eb
yanhuo-zhy <1760331284@qq.com>
2024-05-18 21:21:15 +0800

    wta

--- RankStat/rs.sh
+++ RankStat/rs.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/myocd/temp/rs_CD_CUB2011U.txt
+#SBATCH -o /home/pszzz/hyzheng/myocd/temp/rs_Mollusca.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
@@ -15,8 +15,8 @@ source /home/pszzz/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0
 
-MODEL_PATH='output_cosine/CD_CUB2011U/RankStat&WTA/checkpoints/epoch-best.pth'
-DATASET_NAME='CD_CUB2011U'
+MODEL_PATH='output_cosine/Mollusca/RankStat/checkpoints/epoch-best.pth'
+DATASET_NAME='Mollusca'
 
 python rs_results.py \
  --model_path $MODEL_PATH\
