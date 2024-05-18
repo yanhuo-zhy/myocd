9a29042a1985ac58a6f59176d36cb922463e2616
yanhuo-zhy <1760331284@qq.com>
2024-05-18 10:26:34 +0800

    newsota

--- protop/scripts/train_uon.sh
+++ protop/scripts/train_uon.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/myocd/temp/my_protop_Arachnida_newsota.txt
+#SBATCH -o /home/pszzz/hyzheng/myocd/temp/my_protop_Animalia_newsota.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
@@ -64,8 +64,8 @@ then
 fi
 
 ft=protopformer
-data_set=Arachnida
-prototype_num=280
+data_set=Animalia
+prototype_num=390
 data_path=/db/pszzz/xxx
 
 
