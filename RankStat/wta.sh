4d13a1ff05a7176d4233e5030ba8069985cbce26
yanhuo-zhy <1760331284@qq.com>
2024-05-18 23:03:31 +0800

    wta

--- RankStat/wta.sh
+++ RankStat/wta.sh
@@ -6,7 +6,7 @@
 #SBATCH -c 5
 #SBATCH --mem=20000
 #SBATCH --gres gpu:1
-#SBATCH -o /home/pszzz/hyzheng/myocd/temp/wta_Mollusca.txt
+#SBATCH -o /home/pszzz/hyzheng/myocd/temp/wta_pets.txt
 
 module load gcc/gcc-10.2.0
 module load nvidia/cuda-11.1 nvidia/cudnn-v8.1.1.33-forcuda11.0-to-11.2
@@ -15,8 +15,8 @@ source /home/pszzz/miniconda3/bin/activate zhy
 
 CUDA_VISIBLE_DEVICES=0
 
-MODEL_PATH='output_cosine/Mollusca/WTA/checkpoints/epoch-best.pth'
-DATASET_NAME='Mollusca'
+MODEL_PATH='output_cosine/CD_pets/WTA/checkpoints/epoch-best.pth'
+DATASET_NAME='CD_pets'
 
 python wta.py \
  --model_path $MODEL_PATH\
